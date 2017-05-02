b135b481904776d3a750675bb736330cf5fa0ea9
Savnith <savnith@gmail.com>
2017-05-02 00:51:41 -0400

    Added some logging when it starts

--- src/main/java/net/savcode/fopmr/FrostedOPMod.java
+++ src/main/java/net/savcode/fopmr/FrostedOPMod.java
@@ -3,26 +3,38 @@ package net.savcode.fopmr;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FrostedOPMod extends JavaPlugin implements Listener {
     
     private Connection connection;
     public static FrostedOPMod plugin;
+    PluginDescriptionFile pdf = this.getDescription();
     
     @Override
     public void onLoad() {
         plugin = this;
     }
     
+    @Override
     public void onEnable() {
+        getLogger().log(Level.INFO, "---------------------");
+        getLogger().log(Level.INFO, "FrostedOPMod: R\nversion: {0}\nauthors: {1}", new Object[] { 
+            pdf.getVersion(), pdf.getAuthors() });
+        getLogger().log(Level.INFO, "---------------------");
+        // start stuff
         this.openConnection();
         getConfig().options().copyDefaults(true);
 	saveConfig();
     }
     
+    @Override
     public void onDisable() {
+        
+        // attempt to diable mysql
         try {
             if (connection == null || connection.isClosed())
                 connection.close();
