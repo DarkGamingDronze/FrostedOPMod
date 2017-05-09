a89c3ce75a0db49020eef4c2d5fa80aeed3717e4
DarkGamingDronze <ranad_777@hotmail.com>
2017-05-09 09:08:11 +0200

    This should do the trick

--- src/main/java/net/savcode/fopmr/commands/C_deopall.java
+++ src/main/java/net/savcode/fopmr/commands/C_deopall.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package net.savcode.fopmr.commands;
 
 import java.util.Collection;
 import static net.savcode.fopmr.commands.FOPMR_Command.NO_PERM;
-import static net.savcode.fopmr.commands.FOPMR_Command.OP;
 import net.savcode.fopmr.ranks.Rank;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,10 +10,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-/**
- *
- * @author mhopper81
- */
 public class C_deopall extends FOPMR_Command {
     
     public C_deopall() {
@@ -36,11 +27,11 @@ public class C_deopall extends FOPMR_Command {
         Collection<Player> online = (Collection<Player>) Bukkit.getOnlinePlayers();
         
         if (args.length == 0) {
-            online.forEach((target) -> {
-                target.sendMessage(ChatColor.AQUA + sender.getName() + " - De-opping all players on the server!");
-                target.sendMessage(DEOP);
-                target.setOp(false);
-            });
+            for(Player player: online){
+                player.sendMessage(ChatColor.AQUA + sender.getName() + " - De-opping all players on the server!");
+                player.sendMessage(DEOP);
+                player.setOp(false);
+            }
             return true;
         }
         return true;
